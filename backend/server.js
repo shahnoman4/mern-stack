d76d86cbc3572b56a79834fe71b5e8a6659db3c9
shahnoman4 <shahnoman1001@gmail.com>
2023-12-10 14:42:32 +0500

    Backend api created

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+const port = process.env.PORT || 5000;
+
+await connectDB(); // connect to MongoDB
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Api is runing.......');
+});
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server runing on ${port}`));
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import products from '../products';
 import { Product } from '../components/Product';
 import React from 'react';
+import axios from 'axios';
 
 
 export const HomeScreen = () => {
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() =>{
+       const fetchProducts = async () =>{
+          const { data } = await axios.get('/api/products');
+          setProducts(data);
+       };
+       fetchProducts();
+    },[]);
+
   return (
     <>
       <h1>Latest Products</h1>
